bf02e56da9e41e58c2b173b0dd05149357691463
aryavishnu9 <aryavishnuie@gmail.com>
2023-06-13 15:54:54 -0500

    test

---
+++ Backend/src/Strings_and_exceptions/Intconversion.java
@@ -0,0 +1,24 @@
+package Strings_and_exceptions;
+
+public class Intconversion {
+	static String s;
+	public Intconversion(String s) {
+		this.s=s;
+	}
+	public int test() {
+		return Integer.parseInt(s);
+	}
+
+	public static void main(String[] args) {
+		try {
+		Intconversion i =new Intconversion("test");
+		System.out.println(i.test());
+		}
+		catch(NumberFormatException e){
+			System.out.print("Cannot convert "+s+" to int");
+		}
+		
+
+	}
+
+}
